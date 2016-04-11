39859f590156c4f712ac23b52c20cefc1693c026
Patryk Perduta <patryk.perduta@gmail.com>
2016-04-11 17:42:18 +0000

    remove useless role check in creeps manager

--- manager.creeps.js
+++ manager.creeps.js
@@ -8,16 +8,12 @@ module.exports = () => {
   if (Game.creeps) {
     for (var key in Game.creeps) {
       var c = Game.creeps[key];
-      if(!c.memory.role) {
-        console.log('Creep' + creep + 'has no role!');
-        continue;
-      }
 
-   if(c.memory.role == 'harvester') { doHarvesterWork(c); continue; }
-   if(c.memory.role == 'carrier') { doCarrierWork(c); continue; }
-   if(c.memory.role == 'upgrader') { doUpgraderWork(c); continue; }
-   if(c.memory.role == 'builder') { doBuilderWork(c); continue; }
-   if(c.memory.role == 'attacker') { doAttackerWork(c); continue; }
+     if(c.memory.role == 'harvester') { doHarvesterWork(c); continue; }
+     if(c.memory.role == 'carrier') { doCarrierWork(c); continue; }
+     if(c.memory.role == 'upgrader') { doUpgraderWork(c); continue; }
+     if(c.memory.role == 'builder') { doBuilderWork(c); continue; }
+     if(c.memory.role == 'attacker') { doAttackerWork(c); continue; }
    }
  }
-}
+};
