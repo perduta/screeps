152b00abb1ab3a08bd3c73a56ca27ab4aba6d24d
Patryk Perduta <patryk.perduta@gmail.com>
2016-04-14 22:36:04 +0000

    reworked upgrader role, now works only with upgrader between controller and storage

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,16 +1,42 @@
-module.exports = (c) => {
-  if (c.carry.energy === c.carryCapacity) {
-    c.memory.upgrading = true;
-    c.memory.needsEnergy = false;
-  }
-  else if (c.carry.energy === 0) {
-    c.memory.upgrading = false;
-    c.memory.needsEnergy = true;
-  }
+/* jshint esversion: 6, globalstrict: true */
+"use strict";
 
-  if (c.memory.upgrading) {
-    var ret = c.upgradeController(c.room.controller);
-    if (ret === ERR_NOT_IN_RANGE) c.moveTo(c.room.controller);
-  }
-}
+Creep.prototype.hasSetUp = function() {
+    return this.memory.setUp === true && this.memory.controllerId && this.memory.storageId;
+};
 
+Creep.prototype.setUp = function() {
+    // this version work only if storage is next to upgrader which is next to controller
+    let storages = this.room.find(FIND_MY_STRUCTURES, {filter: e => e.structureType == STRUCTURE_STORAGE});
+    if (storages.length) {
+        let storage = this.pos.findClosestByPath(storages);
+        this.memory.storageId = storage.id;
+    }
+
+    let controller = this.room.controller;
+    this.memory.controllerId = controller.id;
+
+    let upgradePerTick = this.getActiveBodyparts(WORK);
+    this.memory.upgradePerTick = upgradePerTick;
+
+    this.memory.setUp = true;
+    return OK;
+};
+
+module.exports = (creep) => {
+    if (!creep.hasSetUp()) { 
+        creep.setUp(); 
+        if (!creep.hasSetUp()) {
+            return;
+        }
+    }
+ 
+    if (creep.carry.energy <= creep.memory.upgradePerTick + 1) {
+        let storage = Game.getObjectById(creep.memory.storageId);
+        storage.transfer(creep, RESOURCE_ENERGY);
+    }
+
+    let controller = Game.getObjectById(creep.memory.controllerId);
+    let returnCode = creep.upgradeController(controller);
+    if (returnCode == ERR_NOT_IN_RANGE) creep.moveTo(controller);
+};
