1eda5311c9b8f4f2dd0bf96b7f1c4a9b94ce9620
patryk.perduta@gmail.com <patryk.perduta@gmail.com>
2016-04-18 23:09:59 +0200

    changed harvester definition to be bit better

--- creepsDefinitions.js
+++ creepsDefinitions.js
@@ -1,6 +1,6 @@
 module.exports = {
     scout: {requiredParts: [MOVE, WORK, CARRY], optionalParts: []},
-    harvester: {requiredParts: [MOVE, WORK], optionalParts: [MOVE, WORK]},
+    harvester: {requiredParts: [MOVE, WORK, WORK], optionalParts: [MOVE, WORK, WORK]},
     carrier: {requiredParts: [MOVE, CARRY], optionalParts: [MOVE, CARRY]},
     upgrader: {requiredParts: [MOVE, CARRY], optionalParts: [WORK]},
     builder: {requiredParts: [MOVE, CARRY, WORK], optionalParts: [MOVE, CARRY, WORK]},
