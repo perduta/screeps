1c8fb1c442d0106ac54a29f107bc5a1ba793a7e8
patryk.perduta@gmail.com <patryk.perduta@gmail.com>
2016-04-19 05:46:02 +0200

    changed definition of carrier to carry more with cost of movement

--- creepsDefinitions.js
+++ creepsDefinitions.js
@@ -1,7 +1,7 @@
 module.exports = {
-    scout: {requiredParts: [MOVE, WORK, CARRY], optionalParts: []},
+    scout: {requiredParts: [MOVE, WORK, CARRY], optionalParts: [MOVE, WORK, CARRY]},
     harvester: {requiredParts: [MOVE, WORK, WORK], optionalParts: [MOVE, WORK, WORK]},
-    carrier: {requiredParts: [MOVE, CARRY], optionalParts: [MOVE, CARRY]},
+    carrier: {requiredParts: [MOVE, CARRY, CARRY], optionalParts: [MOVE, CARRY, CARRY]},
     upgrader: {requiredParts: [MOVE, CARRY], optionalParts: [WORK]},
     builder: {requiredParts: [MOVE, CARRY, WORK], optionalParts: [MOVE, CARRY, WORK]},
 };
